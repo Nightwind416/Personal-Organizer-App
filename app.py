ca571a8b4044767cb7f3908cfb68a5aaecf3c1f2
Christopher Lorr <christopher.e.lorr@gmail.com>
2023-05-08 00:09:26 -0500

    Simplified to single csv database and started working on template pages

--- app.py
+++ app.py
@@ -20,13 +20,6 @@ def after_request(response):
     return response
 
 
-# Create new empty location database, if one does not exist
-if os.path.isfile('location_database.csv') == False:
-    database = open('location_database.csv', 'w', newline='')
-    database_writer = csv.writer(database)
-    # Add the first row as colum labels
-    database_writer.writerow(['Location Name','Location Type','Detailed Info'])
-    database.close()
 # Create new empty item database, if one does not exist
 if os.path.isfile('item_database.csv') == False:
     database = open('item_database.csv', 'w', newline='')
@@ -82,36 +75,63 @@ def remove():
 @app.route("/update", methods=["GET", "POST"])
 def update():
     if request.method == "POST":
-        database_writer.writerow([item, specific_location, item_type, room_location, detailed_info])
-        organizer_database.flush()
+        # Get item details from form
+        item_name = request.form["name"]
+        item_type = request.form.get("type")
+        item_location = request.form.get("location")
+        item_description = request.form.get("description")
+        # Check if form entries were left blank
+        if not item_name:
+            return apology("You must enter an item name", 400)
+        elif item_name in get_item_name_list():
+            return apology("Sorry, that item name is already used (no duplicates)", 400)
+        elif not item_type:
+            return apology("You must enter an item type", 400)
+        elif not item_location:
+            return apology("You must enter an item location", 400)
+        elif not item_description:
+            return apology("You must enter an item description", 400)
+        # Create new item to add to database
+        new_item = [item_name,item_type,item_location,item_description]
+        # Add new item to database
+        with open('item_database.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(new_item)
+        return render_template("index.html")
     else:
-        return render_template("update.html")
+        item_list = get_item_name_list()
+        return render_template("update.html", item_list=item_list)
 
 
 @app.route("/list", methods=["GET", "POST"])
 def list():
     if request.method == "POST":
-        organizer_database.seek(0)
-        reader = csv.reader(organizer_database)
-        for row in reader:
-            if row[1] == location or row[3] == location:
-                print(row[0])
-        organizer_database.flush()
+        location_name = request.form["location"]
+        if location_name == 'Select a location to narrow your list (leave to list ALL items)':
+            location_list = get_location_name_list()
+        else:
+            location_list = get_location_name_list(location_name)
     else:
-        return render_template("list.html")
+        ...
+        location_list = get_location_name_list()
+    print(location_list)
+    return render_template("list.html", location_list=location_list)
 
 
 @app.route("/search", methods=["GET", "POST"])
 def search():
     if request.method == "POST":
-        organizer_database.seek(0)
-        database_reader = csv.reader(organizer_database)
-        for row in database_reader:
-            if query in row:
-                print(row)
-        organizer_database.flush()
+        ...
     else:
         return render_template("search.html")
+
+
+@app.route("/recycle", methods=["GET", "POST"])
+def recycle():
+    if request.method == "POST":
+        ...
+    else:
+        return render_template("recycle.html")
 
 
 def apology(message, code=400):
@@ -129,13 +149,14 @@ def apology(message, code=400):
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
 
-def get_item_name_list():
+def get_item_name_list(location=None):
     item_names = set()
     with open('item_database.csv', 'r') as csvfile:
         reader = csv.reader(csvfile)
         next(reader)  # skip header row
         for row in reader:
-            item_names.add(row[0])
+            if location is None or row[2] == location:
+                item_names.add(row[0])
     return item_names
 
 
@@ -149,21 +170,11 @@ def get_item_type_list():
     return item_types
 
 
-def get_location_list():
+def get_location_name_list():
     location_names = set()
-    with open('location_database.csv', 'r') as csvfile:
+    with open('item_database.csv', 'r') as csvfile:
         reader = csv.reader(csvfile)
         next(reader)  # skip header row
         for row in reader:
             location_names.add(row[2])
     return location_names
-
-
-def get_location_type_list():
-    location_types = set()
-    with open('location_database.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader)  # skip header row
-        for row in reader:
-            location_types.add(row[1])
-    return location_types
---
+++ test_app.py
@@ -0,0 +1,21 @@
+from app import *
+
+
+def test_add():
+    ...
+
+
+def test_update():
+    ...
+
+
+def test_remove():
+    ...
+
+
+def test_list():
+    ...
+
+
+def test_search():
+    ...
