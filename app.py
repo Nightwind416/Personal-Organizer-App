b1c1754e01b9b298797ea1a04e1a3859e3ee6af5
Christopher Lorr <christopher.e.lorr@gmail.com>
2023-05-17 21:42:15 -0500

    refactored display list and recycle page

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, redirect, render_template, request
 import csv
 import os
+import re
 import urllib.parse
+from datetime import datetime
 
 
 # Configure app
@@ -26,14 +28,7 @@ if os.path.isfile('item_database.csv') == False:
     database = open('item_database.csv', 'w', newline='')
     database_writer = csv.writer(database)
     # Add the first row as colum labels
-    database_writer.writerow(['Item Name', 'Item Type', 'Location', 'Detailed Info'])
-    database.close()
-# Create new empty recycle database, if one does not exist
-if os.path.isfile('recycle_database.csv') == False:
-    database = open('recycle_database.csv', 'w', newline='')
-    database_writer = csv.writer(database)
-    # Add the first row as colum labels
-    database_writer.writerow(['Item Name', 'Item Type', 'Location', 'Detailed Info'])
+    database_writer.writerow(['Item Name', 'Item Type', 'Location', 'Detailed Info', 'Date Added', 'Date Updated', 'Date Recycled'])
     database.close()
 
 
@@ -62,22 +57,139 @@ def add():
         elif not item_description:
             return apology("You must enter an item description", 400)
         # Create new item to add to database
-        new_item = [item_name,item_type,item_location,item_description]
+        date_added = datetime.today().strftime('%Y-%m-%d')
+        date_updated = datetime.today().strftime('%Y-%m-%d')
+        date_recycled = False
+        new_item = [item_name,item_type,item_location,item_description,date_added,date_updated,date_recycled]
         # Add new item to database
         with open('item_database.csv', 'a', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(new_item)
-        return render_template("item_details.html", reason="Newly Added Item", item=new_item)
+        return render_template("item_details.html", reason="New item added", item=new_item)
     else:
         return render_template("add.html")
 
 
-@app.route("/remove", methods=["GET", "POST"])
-def remove():
+@app.route("/list_request", methods=["GET", "POST"])
+def list_request():
     if request.method == "POST":
-        ...
+        location_name = request.form["location_list"]
+        type_name = request.form["type_list"]
+        item_name_list = get_item_name_list(location_name, type_name)
+        print(item_name_list)
+        display_list = build_list_to_display(item_name_list)
+        if len(display_list) == 0:
+            return apology("No items matched your search", 400)
+        return render_template('list_results.html', display_list=display_list)
     else:
-        return render_template("remove.html")
+        location_list = get_location_name_list()
+        type_list = get_item_type_list()
+        return render_template('list_request.html', location_list=location_list, type_list=type_list)
+
+
+@app.route('/item_details', methods=["GET"])
+def items():
+    html_item_name = request.args.get('item_name')
+    item_name = urllib.parse.unquote(html_item_name)
+    item = []
+    with open('item_database.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row['Item Name'] == item_name:
+                item = [row['Item Name'],row['Item Type'],row['Location'],row['Detailed Info'],row['Date Added'],row['Date Updated'],row['Date Recycled']]
+    return render_template("item_details.html", reason="Item Details", item=item)
+
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    if request.method == "POST":
+        search_query = request.form.get("search_query")
+        item_name_list = get_item_name_list()
+        matching_items = []
+        # Perform case-insensitive search for matching items
+        for item_name in item_name_list:
+            with open('item_database.csv', 'r') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    if (search_query.lower() in item_name.lower() or re.search(r"\b{}\b".format(re.escape(search_query.lower())), row['Detailed Info'].lower())):
+                        matching_items.append(row['Item Name'])
+        display_list = build_list_to_display(matching_items)
+        if len(display_list) == 0:
+            return apology("No items matched your search", 400)
+        return render_template("list_results.html", display_list=display_list, search_query=search_query)
+    else:
+        return render_template("search.html")
+
+
+@app.route("/recycle", methods=["GET", "POST"])
+def recycle():
+    if request.method == "POST":
+        rows = []
+        # open and read the database csv into a temp list
+        with open("item_database.csv", "r") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                rows.append(row)
+        # remove recycle flagged items (items with recycle dates)
+        for row in rows:
+            if row["Date Recycled"] != "":
+                rows.remove(row)
+        # write the updated temp list back to the database csv
+        with open("item_database.csv", "w", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=reader.fieldnames)
+            writer.writeheader()
+            writer.writerows(rows)
+        return render_template("index.html")
+    else:
+        item_name_list = get_item_name_list(recycle=True)
+        display_list = build_list_to_display(item_name_list)
+        if len(display_list) == 0:
+            return apology("Recycle bin is empty", 400)
+        return render_template('recycle.html', display_list=display_list)
+
+
+@app.route("/move_to_recycle", methods=["POST"])
+def remove():
+    html_item_name = request.args.get('item_name')
+    item_name = urllib.parse.unquote(html_item_name)
+    rows = []
+    # open and read the database csv into a temp list
+    with open("item_database.csv", "r") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            rows.append(row)
+    # update items date field, to indicate it was 'recycled'
+    for row in rows:
+        if row["Item Name"] == item_name:
+            row["Date Recycled"] = datetime.today().strftime("%Y-%m-%d")
+    # write the updated temp list back to the database csv
+    with open("item_database.csv", "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=reader.fieldnames)
+        writer.writeheader()
+        writer.writerows(rows)
+    return redirect("index.html")
+
+
+@app.route("/remove_from_recycle", methods=["GET", "POST"])
+def remove_from_recycle():
+    html_item_name = request.args.get('item_name')
+    item_name = urllib.parse.unquote(html_item_name)
+    rows = []
+    # open and read the database csv into a temp list
+    with open("item_database.csv", "r") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            rows.append(row)
+    # update items date field, to indicate it was 'un-recycled'
+    for row in rows:
+        if row["Item Name"] == item_name:
+            row["Date Recycled"] = "None"
+    # write the updated temp list back to the database csv
+    with open("item_database.csv", "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=reader.fieldnames)
+        writer.writeheader()
+        writer.writerows(rows)
+    return redirect("/recycle")
 
 
 @app.route("/update_item", methods=["GET", "POST"])
@@ -100,70 +212,27 @@ def update():
         elif not item_description:
             return apology("You must enter an item description", 400)
         # Create new item to add to database
-        updated_item = [item_name,item_type,item_location,item_description]
-        # Add new item to database
-        with open('item_database.csv', 'a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(updated_item)
-        return render_template("item_details.html", reason="Updated Item", item=updated_item)
+        date_updated = datetime.today().strftime('%Y-%m-%d')
+        # Update item in database
+        rows = []
+        with open("item_database.csv", "r") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row["Item Name"] == item_name:
+                    # Update the item with the new details
+                    row["Item Type"] = item_type
+                    row["Location"] = item_location
+                    row["Detailed Info"] = item_description
+                    row["Date Updated"] = date_updated
+                rows.append(row)
+        with open("item_database.csv", "w", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=reader.fieldnames)
+            writer.writeheader()
+            writer.writerows(rows)
+        return render_template("index.html")
     else:
         item_list = get_item_name_list()
         return render_template("update_item.html", item_list=item_list)
-
-
-@app.route("/list_request", methods=["GET", "POST"])
-def list_request():
-    if request.method == "POST":
-        location_name = request.form["location_list"]
-        type_name = request.form["type_list"]
-        item_name_list = get_item_name_list(location_name, type_name)
-        display_list = build_list_to_display(item_name_list)
-        return render_template('list_results.html', display_list=display_list)
-    else:
-        location_list = get_location_name_list()
-        type_list = get_item_type_list()
-        return render_template('list_request.html', location_list=location_list, type_list=type_list)
-
-
-@app.route("/search", methods=["GET", "POST"])
-def search():
-    if request.method == "POST":
-        ...
-    else:
-        return render_template("search.html")
-
-
-@app.route('/item_details', methods=["GET", "POST"])
-def items():
-    if request.method == "POST":
-        ...
-    else:
-        html_item_name = request.args.get('name')
-        print("html_item_name:")
-        print(html_item_name)
-        item_name = urllib.parse.unquote(html_item_name)
-        print(item_name)
-        with open('item_database.csv', 'r') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                if row['Item Name'] == item_name:
-                    item = [row['Item Name'],row['Item Type'],row['Location'],row['Detailed Info']]
-        # item = ['Test Name','Test Type','Test Location','Test Info']
-        return render_template("item_details.html", reason="Details Requested", item=item)
-
-
-@app.route("/recycle", methods=["GET", "POST"])
-def recycle():
-    if request.method == "POST":
-        location_name = request.form["location_list"]
-        type_name = request.form["type_list"]
-        item_name_list = get_item_name_list(location_name, type_name)
-        display_list = build_list_to_display(item_name_list)
-        return render_template('recycle.html', display_list=display_list)
-    else:
-        location_list = get_location_name_list()
-        type_list = get_item_type_list()
-        return render_template('recycle.html', location_list=location_list, type_list=type_list)
 
 
 def apology(message, code=400):
@@ -181,22 +250,32 @@ def apology(message, code=400):
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
 
-def get_item_name_list(location_name=None, type_name=None):
+def get_item_name_list(location_name="None", type_name="None", recycle=False):
+    # print("Location name: " + str(location_name))
+    # print("Type name: " + str(type_name))
+    # print("Recycle: " + str(recycle))
     item_name_list = []
     with open('item_database.csv', 'r') as csvfile:
         reader = csv.DictReader(csvfile)
-        for row in reader:
-            if (location_name == 'None') and (type_name == 'None'):
-                item_name_list.append(row['Item Name'])
-            elif (location_name != 'None') and (type_name == 'None'):
-                if row['Location'] == location_name:
+        if recycle:
+            for row in reader:
+                if row['Date Recycled'] != "None":
                     item_name_list.append(row['Item Name'])
-            elif (location_name == 'None') and (type_name != 'None'):
-                if row['Item Type'] == type_name:
+        else:
+            for row in reader:
+                if row['Date Recycled'] != "None":
+                    continue
+                elif location_name == "None" and type_name == "None":
                     item_name_list.append(row['Item Name'])
-            elif (location_name != 'None') and (type_name != 'None'):
-                if (row['Location'] == location_name) and (row['Item Type'] == type_name):
-                    item_name_list.append(row['Item Name'])
+                elif location_name != "None" and type_name == "None":
+                    if row['Location'] == location_name:
+                        item_name_list.append(row['Item Name'])
+                elif location_name == "None" and type_name != "None":
+                    if row['Item Type'] == type_name:
+                        item_name_list.append(row['Item Name'])
+                elif location_name != "None" and type_name != "None":
+                    if (row['Location'] == location_name) and (row['Item Type'] == type_name):
+                        item_name_list.append(row['Item Name'])
     if len(item_name_list) > 1:
         item_name_list = sorted(set(item_name_list))
     # print(item_name_list)
@@ -228,22 +307,14 @@ def get_location_name_list():
     return location_name_list
 
 
-def build_list_to_display(item_names):
+def build_list_to_display(item_name_list):
     list_to_display = []
     with open('item_database.csv', 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            if row['Item Name'] in item_names:
-                list_to_display.append([row['Item Name'],row['Item Type'],row['Location'],row['Detailed Info']])
+            if row['Item Name'] in item_name_list:
+                list_to_display.append([row['Item Name'],row['Item Type'],row['Location'],row['Detailed Info'],row['Date Added'],row['Date Updated'],row['Date Recycled']])
     if len(list_to_display) > 1:
         list_to_display.sort(key=lambda x: x[0])
     # print(list_to_display)
     return list_to_display
-
-
-def add_to_recycle(item_name):
-    ...
-
-
-def remove_from_recycle(item_name):
-    ...
